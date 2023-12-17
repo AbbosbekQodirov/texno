40c64e280198cbd483a9d41eb16216e30de70c95
Abbosbek <abbosbekqodirov2501@gmail.com>
2023-12-17 15:10:00 +0500

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,49 +8,49 @@ function Home() {
       <div className="container">
         <div className="serviceCard">
           <Link to={"/services/page"} className="card">
-            <img src="../public/imgs/service1.jpg" alt="" />
+            <img src="./assets/imgs/service1.jpg" alt="" />
             <div>
               <h2>Резервуар вертикальный стальной РВС</h2>
             </div>
           </Link>
           <Link to={"/services/page"} className="card">
-            <img src="../public/imgs/service2.jpg" alt="" />
+            <img src="./assets/imgs/service2.jpg" alt="" />
             <div>
               <h2>Резервуары горизонтальные стальные (РГС, РГСН, РГСП)</h2>
             </div>
           </Link>
           <Link to={"/services/page"} className="card">
-            <img src="../public/imgs/service3.jpg" alt="" />
+            <img src="./assets/imgs/service3.jpg" alt="" />
             <div>
               <h2>Модульные АЗС</h2>
             </div>
           </Link>
           <Link to={"/services/page"} className="card">
-            <img src="../public/imgs/service4.jpg" alt="" />
+            <img src="./assets/imgs/service4.jpg" alt="" />
             <div>
               <h2>Автоматическая блочная (мини) АЗС</h2>
             </div>
           </Link>
           <Link to={"/services/page"} className="card">
-            <img src="../public/imgs/service5.jpg" alt="" />
+            <img src="./assets/imgs/service5.jpg" alt="" />
             <div>
               <h2>Контейнерные АЗС (КАЗС)</h2>
             </div>
           </Link>
           <Link to={"/services/page"} className="card">
-            <img src="../public/imgs/service6.jpg" alt="" />
+            <img src="./assets/imgs/service6.jpg" alt="" />
             <div>
               <h2>Силосъг (для цемента, зерна, муки и т.д.)</h2>
             </div>
           </Link>
           <Link to={"/services/page"} className="card">
-            <img src="../public/imgs/service7.jpg" alt="" />
+            <img src="./assets/imgs/service7.jpg" alt="" />
             <div>
               <h2>Емкости подземные и наземные.</h2>
             </div>
           </Link>
           <Link to={"/services/page"} className="card">
-            <img src="../public/imgs/service8.jpg" alt="" />
+            <img src="./assets/imgs/service8.jpg" alt="" />
             <div>
               <h2>
                 Силосы стальные для цемента и муки до 150 м<sup>3</sup>
