f5d9dfb893e59da087cac9998872323e06557593
Abbosbek <abbosbekqodirov2501@gmail.com>
2023-12-17 20:41:11 +0500

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
 import img1 from "../../../public/imgs/service1.jpg"
@@ -11,60 +11,50 @@ import img7 from "../../../public/imgs/service7.jpg";
 import img8 from "../../../public/imgs/service8.jpg";
 
 function Home() {
+  const [data, setData] = useState([])
+  const getData = () => {
+    var requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://teknikinnavatsion.pythonanywhere.com/category/all/",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => {
+        setData(JSON.parse(result).data);
+       })
+      .catch((error) => console.log("error", error));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="home">
       <div className="container">
         <div className="serviceCard">
-          <Link to={"/services/page"} className="card">
-            <img src={img1} alt="" />
-            <div>
-              <h2>Резервуар вертикальный стальной РВС</h2>
-            </div>
-          </Link>
-          <Link to={"/services/page"} className="card">
-            <img src={img2} alt="" />
-            <div>
-              <h2>Резервуары горизонтальные стальные (РГС, РГСН, РГСП)</h2>
-            </div>
-          </Link>
-          <Link to={"/services/page"} className="card">
-            <img src={img3} alt="" />
-            <div>
-              <h2>Модульные АЗС</h2>
-            </div>
-          </Link>
-          <Link to={"/services/page"} className="card">
-            <img src={img4} alt="" />
-            <div>
-              <h2>Автоматическая блочная (мини) АЗС</h2>
-            </div>
-          </Link>
-          <Link to={"/services/page"} className="card">
-            <img src={img5} alt="" />
-            <div>
-              <h2>Контейнерные АЗС (КАЗС)</h2>
-            </div>
-          </Link>
-          <Link to={"/services/page"} className="card">
-            <img src={img6} alt="" />
-            <div>
-              <h2>Силосъг (для цемента, зерна, муки и т.д.)</h2>
-            </div>
-          </Link>
-          <Link to={"/services/page"} className="card">
-            <img src={img7} alt="" />
-            <div>
-              <h2>Емкости подземные и наземные.</h2>
-            </div>
-          </Link>
-          <Link to={"/services/page"} className="card">
-            <img src={img8} alt="" />
-            <div>
-              <h2>
-                Силосы стальные для цемента и муки до 150 м<sup>3</sup>
-              </h2>
-            </div>
-          </Link>
+          {data?.map((item)=>{
+            return (
+              <Link to={"/services/"+item.id} className="card">
+                <img
+                  src={
+                    "https://teknikinnavatsion.pythonanywhere.com/" + item.file
+                  }
+                  alt=""
+                />
+                <div>
+                  <h2>{item.name}</h2>
+                </div>
+              </Link>
+            );
+          })}
+          
         </div>
         <div className="about">
           <h1>о нас</h1>
